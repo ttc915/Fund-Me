78667b296815fb0209f633a70fbdd9bcd3d71fe8
ttc915 <theomaniac917@gmail.com>
2025-07-28 15:15:25 +0300

    another update

--- FundMe.sol
+++ FundMe.sol
@@ -32,7 +32,14 @@ contract FundMe {
     }
 
 
-    // function withdraw() public {}
+    function withdraw() public {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+            payable(funder).transfer(address(this).balance);
+        }
+
+    }
 
 
 
