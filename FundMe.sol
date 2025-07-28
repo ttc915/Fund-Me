5f0f0090eff8af4351da29095c9289751e441875
ttc915 <theomaniac917@gmail.com>
2025-07-28 14:05:37 +0300

    add getPrice

--- FundMe.sol
+++ FundMe.sol
@@ -11,13 +11,13 @@ import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interf
 
 contract FundMe {
 
-    uint256 public minimumUsd = 5;
+    uint256 public minimumUsd = 5 * 1e18;
 
     function fund() public payable {
         // allow users to send $
         // have a minimum of $5 sent
         // 1. how do we send ETH to this contract?
-        require( msg.value >= minimumUsd, "Didn't sent enough ETH"); // 1e18 = 1 ETH
+        require( getConversionRate(msg.value) >= minimumUsd, "Didn't sent enough ETH"); // 1e18 = 1 ETH
 
         // What is a revert
         // undi any action that have been done
@@ -26,15 +26,28 @@ contract FundMe {
 
     // function withdraw() public {}
 
-    function getPrice() public  {
+    function getPrice() public view returns (uint256) {
         // address 0x694AA1769357215DE4FAC081bf1f309aDC325306
         // abi
+        address priceFeedAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(priceFeedAddress);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        // prie of ETH in USD
+        // 3800.000000000
+        return uint256(price * 1e10);
+    }
+
+    function getConversionRate(uint256 ethAmmount) public view returns (uint256) {
+        uint256 price = getPrice();
+        uint256 priceInUsd = (price * ethAmmount) / 1e18;
+        return priceInUsd;
 
     }
 
-    function getConversionRate() public  {
-
+    function getVersion() public view returns (uint256) {
+        address priceFeedAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(priceFeedAddress);
+        return priceFeed.version();
     }
-
 
 }
