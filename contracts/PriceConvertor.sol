8a15b63063a32d36de3c9ed3853bbe6bf212f786
ttc915 <theomaniac917@gmail.com>
2025-07-29 13:27:57 +0300

    move to contact

--- contracts/PriceConvertor.sol
+++ contracts/PriceConvertor.sol
@@ -1,5 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.24;
+
+
+// 1. Limit self-triage to 15/20 minutes
+// 2. Don't be afraid to ask AI, but don't skip learning
+// "Hallucinations" 
+// 3. Use the forums!!!
+// 4. Google the exact error
+// 5. Post in stack exchange or peeranhe
+
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
@@ -24,6 +33,7 @@ library PriceConverter {
 
     function getVersion() internal view returns (uint256) {
         address priceFeedAddress = 0x694AA1769357215DE4FAC081bf1f309aDC325306;
+        // address priceFeedAddress = 0xfEefF7c3fB57d18C5C6Cdd71e45D2D0b4F9377bF;
         AggregatorV3Interface priceFeed = AggregatorV3Interface(priceFeedAddress);
         return priceFeed.version();
     }
